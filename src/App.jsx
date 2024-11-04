32f241af96aa680808f85307adb237ce6d990402
shoyonp <shoyonkumar143@gmail.com>
2024-11-04 22:23:13 +0600

    set up done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button className="btn btn-primary">ok</button>
+    </>
+  );
+}
+
+export default App;
